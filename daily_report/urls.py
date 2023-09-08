475808e9499b54fd3010078c9809f36d7b49b06d
hayato <nanalove.hayato1220@icloud.com>
2023-09-09 01:17:30 +0900

    作業ページ作成

--- daily_report/urls.py
+++ daily_report/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import ReportListView
 
 app_name =  'daily_report'
 
 urlpatterns = [
-    
+    path('report_list', ReportListView.as_view, name='report_list'),
 ]
--- daily_report/views.py
+++ daily_report/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.views.generic.list import ListView
 
+from accounts.models import Users
+import os
 
 #ログイン状態
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,5 +11,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 #作業一覧
-class WorkListView(LoginRequiredMixin, ListView):
-    pass
+class ReportListView(LoginRequiredMixin, ListView):
+    model = Users
+    template_name = os.path.join('daily_report', 'report/report_list.html')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_department_name'] = self.request.GET.get('user_department_name', None)
+        return context
