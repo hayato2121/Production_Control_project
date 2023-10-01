57d723983abb70b08d24c41695a60052f693aee0
hayato <nanalove.hayato1220@icloud.com>
2023-10-01 18:11:27 +0900

    デプロイ

--- production_control_project/settings.py
+++ production_control_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@bs80h!0%y8mnkl^so3c^te!ws4fxy3xyh2h6qrvgo1@2yvr=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://hayato.pythonanywhere.com/']
 
 
 # Application definition
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFIELS_DIR = [
     STATIC_DIR,
 ]
+
 
 
 # Default primary key field type
