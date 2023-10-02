11cc4c687068c710ba6a480105083ab94383131f
hayato <nanalove.hayato1220@icloud.com>
2023-10-03 00:36:28 +0900

    フォント確認

--- product_management/views.py
+++ product_management/views.py
@@ -112,6 +112,22 @@ class StaffReportUserGraphView(View):
                 plt.bar(x, product_good_data, label=product.name, bottom=bottom_data)
                 bottom_data += product_good_data
 
+                        # フォントを全て読み込み
+            fonts = set([f.name for f in matplotlib.font_manager.fontManager.ttflist])
+            
+            # 描画領域のサイズ調整
+            plt.figure(figsize=(10,len(fonts)/4))
+            
+            # フォントの表示
+            for i, font in enumerate(fonts):
+                plt.text(0, i, f"日本語：{font}", fontname=font)
+                
+            # 見やすいように軸を消す
+            plt.ylim(0, len(fonts))
+            plt.axis("off")
+                
+            plt.show()
+
             plt.xlabel('ユーザー',fontname = 'Arial Unicode MS')
             plt.ylabel('優良数',fontname = 'Arial Unicode MS')
             plt.xticks(x, labels, rotation=45)
