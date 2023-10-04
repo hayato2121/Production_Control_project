dbb5a54d027deff3953e07f637b07d82efe06a50
hayato <nanalove.hayato1220@icloud.com>
2023-10-04 23:15:35 +0900

    email設定

--- production_control_project/settings.py
+++ production_control_project/settings.py
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'nanalove.hayato1220@gmail.com'
+EMAIL_HOST_PASSWORD = 'raynekomimi21'
 
 
 #ログイン後の設定
